013559b550288b828a7e004dd2e4cb906e371b7c
Zipho Zonke Maphumulo <zipho@example.com>
2025-08-28 15:03:54 +0200

    Initial commit - HPSA App

---
+++ app/src/main/java/io/github/ziphozonkemaphumulo83_svg/twa/DelegationService.java
@@ -0,0 +1,18 @@
+package io.github.ziphozonkemaphumulo83_svg.twa;
+
+
+import com.google.androidbrowserhelper.playbilling.digitalgoods.DigitalGoodsRequestHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new DigitalGoodsRequestHandler(getApplicationContext()));
+        
+    }
+}
+
