18a585ae9a58c9e641f62e4ed11f093e76b99dac
Zipho Zonke Maphumulo <zipho@example.com>
2025-08-28 17:51:44 +0200

    Add offline.html and update service worker for offline fallback

---
+++ app/src/main/java/io/github/ziphozonkemaphumulo83_svg/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.ziphozonkemaphumulo83_svg.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
